47ff8450754ef5ce97005e72aee47db36c731bfd
AMD <tdodynho9@gmail.com>
2026-05-01 16:32:25 -0300

    uptade: atualizados campos de reservas, onde vem o nome do cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -3,7 +3,7 @@ package model;
 // Classe concreta que herda as características de Pessoa, representando um cliente do sistema.
 public class Cliente extends Pessoa {
 
-    // Construtor que repassa os dados recebidos para a classe mãe (Pessoa) através do super().
+	  // Construtor que repassa os dados recebidos para a classe mãe (Pessoa) através do super().
     public Cliente(String nome, String cpf, String telefone) {
         super(nome, cpf, telefone);
     }
